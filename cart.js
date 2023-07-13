a04051af6c7922f210220be91f9037fed3711518
kaiserfaravi <shahidullahkaiserfaravi@gmail.com>
2023-07-13 18:47:44 +0600

    first commit

---
+++ cart.js
@@ -0,0 +1,46 @@
+const getInputValueById =id=>{
+const inputField= document.getElementById(id);
+const inputValue = inputField.value;
+inputField.value ='';
+return inputValue;
+};
+
+const addProduct =()=>{
+        const product = getInputValueById( 'product-name');
+        const quantity = getInputValueById( 'product-quantity');
+        console.log(product,quantity);
+        //display product
+        displayProduct(product,quantity);
+        //set to localstorage
+        //simple way
+        // localStorage.setItem(product,quantity);
+        saveItemOnLocalStorage(product,quantity);
+
+}
+
+const getShoppingCartFromLocalStorage = ()=>{
+            let savedCart = localStorage.getItem('cart');
+            let cart ={};
+            if(savedCart){
+                cart = JSON.parse(savedCart);
+
+            }
+            return cart;
+}
+const saveItemOnLocalStorage =(product,quantity)=>{
+        const cart = getShoppingCartFromLocalStorage();
+        //add product as the property
+        cart[product]=quantity;
+        const cartStringified = JSON.stringify(cart);
+        //save to localStorage
+        localStorage.setItem('cart',cartStringified);
+
+
+    }
+
+const displayProduct =(product,quantity)=>{
+        const productContainer =document.getElementById('product-container');
+        const li = document.createElement('li');
+        li.innerText =`${product}:${quantity}`;
+        productContainer.appendChild(li);
+}
